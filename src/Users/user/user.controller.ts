65d055b723b1feabca42e239134372c509f97ea2
Bryan <bryangodinez00@gmail.com>
2022-10-03 14:46:58 -0600

    git init

---
+++ src/Users/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { User } from 'src/models/User';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private userService : UserService ){
+
+    }
+    @Post()
+    Create(@Body() params : User ):void{
+        this.userService.create(params)
+    }
+    @Get('/all')
+    getUsers(): User[] {
+        return this.userService.getAll()
+    }
+    @Get('/:correo')
+    getUser(@Param('correo') param) : User | string{
+        const user = this.userService.getByEmail(param)
+        return user ?? "El usuario no existe"
+    }
+    @Put('/update/:id')
+    actualizartUsuario(@Body() user : User, @Param('id') id){
+        return this.userService.updateUserById(Number(id), user)
+    }
+}
---
+++ src/Users/user/user.service.ts
@@ -0,0 +1,37 @@
+import { User } from './../../models/User';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    private readonly Users: User[] = []
+
+    create( user : User ): void{
+        this.Users.push(user)
+    }
+
+    getAll() : User[]{
+        return this.Users
+    }
+
+    getByEmail(correo : string) : User{
+        return this.Users.find( (user) => user.correo === correo )
+    }
+
+    updateUserById(id : number, user : User) : boolean {
+        let user_index = this.Users.findIndex( (user) => user.id === id )
+
+        if(user_index !== -1 ){
+            //mantener los datos que no se van a actualizar
+            this.Users[user_index] = {
+                id : user.id,
+                nombre : user.nombre,
+                correo : user.correo,
+                telefono : user.telefono
+            }
+
+            return true
+            
+        }
+        return false
+    }
+}
